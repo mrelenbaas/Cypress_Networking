48dc94780c81e381abee653b00c93042d7e15405
Bradley Elenbaas <mr.elenbaas@gmail.com>
2019-12-19 17:07:24 -0600

    Added find_my_ip_address() method to C++ UDP application.

--- Windows_UDP/udp_client/udp_client/udp_client/udp_client.cpp
+++ Windows_UDP/udp_client/udp_client/udp_client/udp_client.cpp
@@ -5,10 +5,65 @@
 #include <ws2tcpip.h>
 #include <string>
 
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <array>
+
 #pragma comment (lib, "ws2_32.lib")
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+std::string find_my_ip_address()
+{
+    //std::string address;
+    //std::system("ipconfig");
+    std::string result = exec("ipconfig");
+
+    std::cout << "HERE HERE HERE" << std::endl;
+    //std::string s = "scott>=tiger>=mushroom";
+    std::string s = result;
+    std::string delimiter = "\n";
+    size_t pos = 0;
+    int count = 0;
+    std::string token;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        //std::cout << count << std::endl;
+        ++count;
+        //std::cout << token << std::endl;
+        if ((token.find("IPv4 Address") != std::string::npos)
+            && (token.find("192.168.1.") != std::string::npos))
+        {
+            std::string address = token.replace(token.begin(), token.begin() + 39, "");
+            //std::string address2 = address.replace(address.end()-1, address.end(), "");
+            //std::cout << count << std::endl;
+            std::cout << address << std::endl;
+            return address;
+        }
+        s.erase(0, pos + delimiter.length());
+    }
+    //std::cout << "HERE HERE HERE" << std::endl;
+    //std::cout << result << std::endl;
+    //return address;
+}
 
 int main(int argc, char* argv[])
 {
+    find_my_ip_address();
+
+    /*
     WSADATA data;
     WORD version = MAKEWORD(2, 2);
     int wsOK = WSAStartup(version, &data);
@@ -35,4 +90,5 @@ int main(int argc, char* argv[])
 
     closesocket(out);
     WSACleanup();
+    */
 }
